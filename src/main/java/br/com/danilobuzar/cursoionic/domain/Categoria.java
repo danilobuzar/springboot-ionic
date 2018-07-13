ad9f98144d347c667682463e4193c8931ecf947e
Danilo <danilobuzar@yahoo.com.br>
2018-07-13 13:03:53 -0300

    alteração da classe de dominio e adição do banco de dados H2

--- src/main/java/br/com/danilobuzar/cursoionic/domain/Categoria.java
+++ src/main/java/br/com/danilobuzar/cursoionic/domain/Categoria.java
@@ -2,12 +2,22 @@ package br.com.danilobuzar.cursoionic.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = -5659300777796473086L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column
 	private String nome;
 
 	public Categoria() {
