793d3dcf32cb4e62e023760da93d8c655dba81ba
Danilo <danilobuzar@yahoo.com.br>
2018-07-13 12:27:21 -0300

    Adição da classe de dominio

---
+++ src/main/java/br/com/danilobuzar/cursoionic/domain/Categoria.java
@@ -0,0 +1,69 @@
+package br.com.danilobuzar.cursoionic.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = -5659300777796473086L;
+
+	private Integer id;
+
+	private String nome;
+
+	public Categoria() {
+		super();
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		final Categoria other = (Categoria) obj;
+		if (this.id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!this.id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+}
--- src/main/java/br/com/danilobuzar/cursoionic/resources/CategoriaResource.java
+++ src/main/java/br/com/danilobuzar/cursoionic/resources/CategoriaResource.java
@@ -1,20 +1,28 @@
 package br.com.danilobuzar.cursoionic.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.danilobuzar.cursoionic.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	private Integer id;
-	
-	private String nome;
-	
 	@GetMapping
-	public String list() {
-		return "REST funcionando";
+	public List<Categoria> list() {
+		final Categoria cat1 = new Categoria(1, "Informatica");
+		final Categoria cat2 = new Categoria(2, "Escritório");
+
+		final List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
+
 }
